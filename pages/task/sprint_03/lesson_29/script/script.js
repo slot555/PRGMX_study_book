0447bf6446ac57b75ef2d1015994590591d9aabe
slot555 <mymaine555@gmail.com>
2022-02-06 08:20:41 +0200

    asda

---
+++ pages/task/sprint_03/lesson_29/script/script.js
@@ -0,0 +1,23 @@
+// Блок сообщений
+
+
+
+console.log('Console view');
+
+let x = 'Hello',
+    probel = ' ',
+    y = 'world',
+    
+
+z = x + probel + y;
+
+alert(z);
+
+let a = 8;
+
+z = z + a;
+
+let rez1 = 5, rez2 = '3';
+let rez = rez1 + rez2;
+
+console.log(rez);
